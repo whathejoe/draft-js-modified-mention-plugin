1b31610ff4b71b759d0280b33094640ffaac95bc
Ceferino Villareal IV <clvillarealiv@gmail.com>
2016-11-23 12:07:01 +0800

    Add modified regex

--- src/mentionSuggestionsStrategy.js
+++ src/mentionSuggestionsStrategy.js
@@ -4,5 +4,5 @@ import findWithRegex from 'find-with-regex';
 import escapeRegExp from 'lodash.escaperegexp';
 
 export default (trigger: String, regExp: String) => (contentBlock: Object, callback: Function) => {
-  findWithRegex(new RegExp(`${escapeRegExp(trigger)}${regExp}`, 'g'), contentBlock, callback);
+  findWithRegex(new RegExp(`(\\s|^|.)${escapeRegExp(trigger)}${regExp}`, 'g'), contentBlock, callback);
 };
